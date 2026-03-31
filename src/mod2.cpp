38d4da058f22d44b93d7c444f1ea19a5e5e5b93c
Liminal studio <gliglatylegrand@gmail.com>
2026-03-31 15:45:21 +0200

    Update mod2.cpp

--- src/mod2.cpp
+++ src/mod2.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <fmt/format.h>
 using namespace geode::prelude;
 
 static int64_t s_sessionStart = 0;
@@ -10,69 +11,79 @@ public:
         return Mod::get()->getSavedValue<int64_t>("accumulated-seconds", 0);
     }
 
-    static void addSeconds(int64_t s) {
-        Mod::get()->setSavedValue("accumulated-seconds", getAccumulatedSeconds() + s);
+    static void addSeconds(int64_t sec) {
+        int64_t nowValue = getAccumulatedSeconds();
+        Mod::get()->setSavedValue("accumulated-seconds", nowValue + sec);
     }
 
     static std::string formatDuration(int64_t t) {
-        int d = t / 86400, h = (t % 86400) / 3600, m = (t % 3600) / 60, s = t % 60;
-        std::string o;
-        if(d>0)o+=std::to_string(d)+"d ";
-        if(h>0||d>0)o+=std::to_string(h)+"h ";
-        o+=std::to_string(m)+"m "+std::to_string(s)+"s";
-        return o;
+        int d = t / 86400;
+        int h = (t % 86400) / 3600;
+        int m = (t % 3600) / 60;
+        int s = t % 60;
+        std::string out;
+        if(d) out += std::to_string(d) + "d ";
+        if(h || d) out += std::to_string(h) + "h ";
+        out += std::to_string(m) + "m " + std::to_string(s) + "s";
+        return out;
     }
 };
 
 class SessionSaver : public CCNode {
 public:
-    static SessionSaver* create() {
-        auto r = new SessionSaver();
-        if(r&&r->init()){r->autorelease();return r;}
-        delete r;
-        return nullptr;
-    }
-
     bool init() {
-        if(!CCNode::init())return false;
+        if(!CCNode::init()) return false;
         this->schedule(schedule_selector(SessionSaver::tick), 30.f);
         return true;
     }
 
     void tick(float) {
         auto pl = PlayLayer::get();
-        if(!pl) return;
-        if(s_sessionStart<=0) return;
+        if(!pl || s_sessionStart <= 0) return;
+
         auto now = static_cast<int64_t>(std::time(nullptr));
         auto diff = now - s_sessionStart;
-        if(diff>0){
+        if(diff > 0) {
             TimeTracker::addSeconds(diff);
             s_sessionStart = now;
         }
     }
+
+    static SessionSaver* create() {
+        auto s = new SessionSaver();
+        if(s && s->init()) { s->autorelease(); return s; }
+        delete s;
+        return nullptr;
+    }
 };
+
+$on_mod(Loaded){
+    s_sessionStart = static_cast<int64_t>(std::time(nullptr));
+    TimeTracker::getAccumulatedSeconds();
+    log::info("Screen Time ready");
+}
 
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
-        if(!MenuLayer::init())return false;
+        if(!MenuLayer::init()) return false;
 
-        auto menu=this->getChildByID("bottom-menu");
-        if(!menu)return true;
+        auto menu = this->getChildByID("bottom-menu");
+        if(!menu) return true;
 
-        if(!this->getChildByID("screentime-saver"_spr)){
-            auto saver=SessionSaver::create();
+        if(!this->getChildByID("screentime-saver"_spr)) {
+            auto saver = SessionSaver::create();
             saver->setID("screentime-saver"_spr);
             this->addChild(saver);
         }
 
-        auto tex=CCTextureCache::sharedTextureCache()->addImage("icon.png", false);
-        if(!tex)return true;
+        auto tex = CCTextureCache::sharedTextureCache()->addImage("icon.png", false);
+        if(!tex) return true;
 
-        auto spr=CircleButtonSprite::create();
+        auto spr = CircleButtonSprite::create(menu, CircleBaseColor::Green, CircleBaseSize::Medium);
         spr->setTexture(tex);
         spr->setScale(0.7f);
 
-        auto btn=CCMenuItemSpriteExtra::create(spr,this,menu_selector(MyMenuLayer::onTime));
+        auto btn = CCMenuItemSpriteExtra::create(spr, this, menu_selector(MyMenuLayer::onTime));
         btn->setID("time-button"_spr);
         menu->addChild(btn);
         menu->updateLayout();
@@ -80,20 +91,20 @@ class $modify(MyMenuLayer, MenuLayer) {
         return true;
     }
 
-    void onTime(CCObject*){
-        auto pl=PlayLayer::get();
-        if(!pl){
+    void onTime(CCObject*) {
+        auto pl = PlayLayer::get();
+        if(!pl) {
             FLAlertLayer::create("Screen Time","Start a level to track playtime.","OK")->show();
             return;
         }
-        auto t=TimeTracker::getAccumulatedSeconds();
-        if(s_sessionStart>0) t+=static_cast<int64_t>(std::time(nullptr))-s_sessionStart;
-        FLAlertLayer::create("Screen Time",fmt::format("Playtime: {}",TimeTracker::formatDuration(t)).c_str(),"OK")->show();
+
+        auto t = TimeTracker::getAccumulatedSeconds();
+        if(s_sessionStart > 0) t += static_cast<int64_t>(std::time(nullptr)) - s_sessionStart;
+
+        FLAlertLayer::create(
+            "Screen Time",
+            fmt::format("Playtime: {}", TimeTracker::formatDuration(t)).c_str(),
+            "OK"
+        )->show();
     }
 };
-
-$on_mod(Loaded){
-    s_sessionStart=static_cast<int64_t>(std::time(nullptr));
-    TimeTracker::getAccumulatedSeconds();
-    log::info("Screen Time ready");
-}
